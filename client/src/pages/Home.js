9c73a4ef7c2c8892c9b0be0d392c685bd1664fd5
Kona Tess <konatess@gmail.com>
2019-03-01 11:35:32 -0800

    Changed text color.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -85,7 +85,7 @@ class Home extends Component {
                 <List>
                   {this.state.books.map(book => (
                     <Book
-                      className={"text-black"}
+                      className={"text-dark"}
                       key={book.id}
                       title={book.volumeInfo.title}
                       subtitle={book.volumeInfo.subtitle}
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -50,6 +50,7 @@ class Saved extends Component {
                 <List>
                   {this.state.books.map(book => (
                     <Book
+                      className={"text-dark"}
                       key={book._id}
                       title={book.title}
                       subtitle={book.subtitle}
